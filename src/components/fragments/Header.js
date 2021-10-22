14fa150f1ce5e8f82370bba31e22862853229ddc
yeaw <yeawyow@gmail.com>
2021-10-22 13:51:12 +0700

    logo

--- src/components/fragments/Header.js
+++ src/components/fragments/Header.js
@@ -1,5 +1,22 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Header() {
-  return <div>โรงพยาบาลอากาศอำนวย</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
+            โรงพยาบาลอากาศอำนวย
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
